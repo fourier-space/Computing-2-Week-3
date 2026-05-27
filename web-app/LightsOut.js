7d4501c9f7f64479fddbd33579577d1cfc43e144
fourier-space <freddie.page@imperial.ac.uk>
2026-05-27 12:07:33 +0100

    post-session update

--- web-app/LightsOut.js
+++ web-app/LightsOut.js
@@ -10,41 +10,28 @@ LightsOut.new_level = function () {
     ];
 };
 
+const stencil = function (
+    state_row_index,
+    state_column_index,
+    flip_row_index,
+    flip_column_index
+) {
+    return (
+        Math.abs(state_row_index - flip_row_index) +
+        Math.abs(state_column_index - flip_column_index) <=
+        1
+    );
+};
+
 LightsOut.flip = function (flip_row_index, flip_column_index, state) {
     return state.map(function (row, state_row_index) {
         return row.map(function (cell, state_column_index) {
-            if (
-                state_row_index === flip_row_index &&
-                state_column_index === flip_column_index
-            ) {
-                return !cell;
-            }
-
-            if (
-                state_row_index === flip_row_index + 1 &&
-                state_column_index === flip_column_index
-            ) {
-                return !cell;
-            }
-
-            if (
-                state_row_index === flip_row_index - 1 &&
-                state_column_index === flip_column_index
-            ) {
-                return !cell;
-            }
-
-            if (
-                state_row_index === flip_row_index &&
-                state_column_index === flip_column_index + 1
-            ) {
-                return !cell;
-            }
-
-            if (
-                state_row_index === flip_row_index &&
-                state_column_index === flip_column_index - 1
-            ) {
+            if (stencil(
+                state_row_index,
+                state_column_index,
+                flip_row_index,
+                flip_column_index
+            )) {
                 return !cell;
             }
 
