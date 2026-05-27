6f2ae64a7a7e18bcf00ad21d981d40d19275313c
fourier-space <freddie.page@imperial.ac.uk>
2026-05-27 09:52:49 +0100

    Week 4.3

--- web-app/main.js
+++ web-app/main.js
@@ -1,5 +1,28 @@
-import Connect4 from "Connect4.js";
+// import Connect4 from "./Connect4.js";
+import R from "./ramda.js";
 
-// Table things, stuff for the UI.
+const row_count = 3;
+const column_count = 3;
 
-// Use the Connect 4 module;
+document.documentElement.style.setProperty("--row-count", row_count);
+document.documentElement.style.setProperty("--column-count", column_count);
+
+const game_grid = document.getElementById("game-grid");
+
+R.range(0, row_count).forEach(function (row_index) {
+    const tr = document.createElement("tr");
+    game_grid.append(tr);
+
+    R.range(0, column_count).forEach(function (column_index) {
+        const td = document.createElement("td");
+        tr.append(td);
+        td.textContent = `[${row_index},${column_index}]`;
+
+        td.onclick = function () {
+            document.querySelector("aside").textContent = (
+                `Row: ${row_index}, Column: ${column_index}`
+            );
+        };
+    });
+});
+
